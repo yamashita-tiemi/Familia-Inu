1a3e51bee5610e4366a71e61d80c7b75ffdd03c5
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2024-09-23 17:35:06 -0300

    Preper merge branchs

--- src/app/_components/icon.tsx
+++ src/app/_components/icon.tsx
@@ -1,6 +1,7 @@
 import { Center, HStack, Link, Stack } from "@chakra-ui/react";
-import { FaSquareWhatsapp, FaSquareInstagram, FaTiktok, FaWhatsapp, FaInstagram } from "react-icons/fa6";
-import { IoPawSharp } from "react-icons/io5";
+import {FaTiktok, FaWhatsapp, FaInstagram } from "react-icons/fa"
+
+//import { IoPawSharp } from "react-icons/io5";
 import NextLink from "next/link";
 import { TextIndex } from "./text";
 
@@ -12,7 +13,7 @@ interface IconCardProps {
 export function IconCard({ opacity }: IconCardProps) {
     return (
         <>
-            <IoPawSharp size={"28px"} color="#CB4817" opacity={opacity} />
+            
         </>
     )
 }
@@ -22,13 +23,13 @@ export function IconSocial() {
         <Stack width={"20%"}>
             <Link as={NextLink} href={'https://web.whatsapp.com/'} isExternal={true}>
                 <HStack margin="0 auto">
-                    <FaSquareWhatsapp size={"30px"} color="#FFFFFF" />
+               
                     <TextIndex margin="0 auto" text={"(32) 9 XXXX-XXXX"} size={"16px"}  color={"#FFFFFF"} />
                 </HStack>
             </Link>
             <Link as={NextLink} href={'https://www.instagram.com/'} isExternal={true}>
                 <HStack>
-                    <FaSquareInstagram size={"30px"} color="#FFFFFF" />
+
                     <TextIndex margin="0 auto" text={"@familia_inu"} size={"16px"} color={"#FFFFFF"} />
                 </HStack>
             </Link>
