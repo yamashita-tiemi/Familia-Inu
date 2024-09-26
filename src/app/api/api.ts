6f54a9e59482f1330372d09430c3f6f43bc66067
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-09-26 17:23:52 -0300

    ok

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8083/'
+    baseURL: 'https://familia-inu-service-ts-152415784515.us-east1.run.app'
 })
 
 export default instance;
